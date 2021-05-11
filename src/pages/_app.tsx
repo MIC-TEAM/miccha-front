4e59027b38c59451f3b21fbd96b4a875d0ccb1d1
devho813 <devho813@gmail.com>
2021-05-11 14:36:47 +0900

    메타 태그 적용 및 글로벌 폰트 적용

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,23 @@
 import type { AppProps } from 'next/app'
 import { RecoilRoot } from 'recoil'
 import { GlobalStyles } from '../utils/GlobalStyles'
+import Head from 'next/head'
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <title>믹챠</title>
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=0"
+        />
+
+        <meta
+          name="description"
+          content="모든 영화, 드라마, 다큐멘터리, 애니메이션을 언제 어디서나 최고의 화질로 무제한 감상하세요."
+        />
+      </Head>
       {GlobalStyles}
       <RecoilRoot>
         <Component {...pageProps} />
---
+++ src/pages/_document.tsx
@@ -0,0 +1,30 @@
+import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document'
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <meta name="author" content="MIC-TEAM" />
+          <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;700&family=Roboto:wght@300;400;700&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
