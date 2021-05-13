b9263db0d94aa13012271636b00da8e1c709825b
leemember <hyunmail521@gmail.com>
2021-05-13 13:56:27 +0900

    remove: background type optional

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -6,14 +6,13 @@ import Image from 'next/image'
 type Props = {
   title: string
   desc: string
-  background?: string
+  background: string
 }
 
 const Main = ({ title, desc, background }: Props) => {
   return (
     <MainSection className="section">
       <Image src={background} alt="믹챠 메인페이지 배경사진" layout="fill" objectFit="cover" />
-
       <h2>{title}</h2>
       <p>{desc}</p>
       <StartBtn type="button">2주 무료 이용 시작</StartBtn>
