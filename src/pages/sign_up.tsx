b3b33cceafbecdbdc9ec2a6e7916ffa4ac8e6940
이현주 <hyunmail521@gmail.com>
2021-05-23 12:24:06 +0900

    fix : 유효성검증 error 구현

--- src/pages/sign_up.tsx
+++ src/pages/sign_up.tsx
@@ -25,9 +25,12 @@ const SignUp: React.FC = () => {
               password: '',
             }}
             validationSchema={Yup.object().shape({
-              name: Yup.string().min(2, '너무 짧습니다.').max(20, '너무 깁니다.').required(),
-              email: Yup.string().email('이메일을 정확히 입력하세요').min(8, '너무 짧습니다.').required(),
-              password: Yup.string().min(10, '너무 짧습니다.').required(),
+              name: Yup.string().min(2).max(20).required(),
+              email: Yup.string().email().required(),
+              password: Yup.string()
+                .min(10)
+                .matches(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{10,}$/)
+                .required(),
             })}
             onSubmit={(values, { setSubmitting }) => {
               setTimeout(() => {
@@ -41,12 +44,12 @@ const SignUp: React.FC = () => {
                 <InputBox>
                   <div className="inputBox name">
                     <Field type="name" placeholder="이름 (2자 이상)" name="name" className="input" />
-                    <ErrorMessage name="name" component="div" className="err" />
+                    <ErrorMessage name="name" component="span" className="error" />
                   </div>
 
                   <div className="inputBox email">
                     <Field type="email" name="email" placeholder="이메일 (example@gamil.com)" className="input" />
-                    <ErrorMessage name="email" component="div" className="err" />
+                    <ErrorMessage name="email" component="span" className="error" />
                   </div>
 
                   <div className="inputBox password">
@@ -56,7 +59,7 @@ const SignUp: React.FC = () => {
                       placeholder="영문, 숫자, 특문 중 2개 조합 10자 이상"
                       className="input"
                     />
-                    <ErrorMessage name="password" component="div" className="err" />
+                    <ErrorMessage name="password" component="span" className="error" />
                   </div>
 
                   <strong className="duplication">이미 존재하는 이메일입니다.</strong>
