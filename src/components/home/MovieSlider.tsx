391d1a9bb269e0c5434951e691a962fb9ff79295
leemember <hyunmail521@gmail.com>
2021-05-26 16:36:51 +0900

    add : HOME page + movieSlider components

---
+++ src/components/home/MovieSlider.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <>
+      <h1>므비</h1>
+    </>
+  )
+}
+
+export default Home
---
+++ src/pages/home.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import MovieSlider from '../components/home/MovieSlider'
+
+const Home = () => {
+  return (
+    <>
+      <MovieSlider />
+    </>
+  )
+}
+
+export default Home
