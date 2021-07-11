a67ec509ec5969e86fea795317172c4684c02a8e
이현주 <hyunmail521@gmail.com>
2021-07-11 16:52:37 +0900

    fix : 화살표 위치값

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -7,16 +7,16 @@ const Footer = () => {
       <div>
         <p>
           주식회사 믹챠 | 대표
-          <a href="https://github.com/devho813" target="_blank">
+          <a href="https://github.com/devho813" target="_blank" rel="noreferrer">
             이찬호
           </a>
-          <a href="https://github.com/leemember" target="_blank">
+          <a href="https://github.com/leemember" target="_blank" rel="noreferrer">
             이현주
           </a>
-          <a href="https://github.com/youmustgo" target="_blank">
+          <a href="https://github.com/youmustgo" target="_blank" rel="noreferrer">
             배일섭
           </a>
-          <a href="https://github.com/Roharui" target="_blank">
+          <a href="https://github.com/Roharui" target="_blank" rel="noreferrer">
             김태윤
           </a>
           <span>| 대한민국 이곳 저곳</span>
@@ -39,7 +39,7 @@ const FooterWrap = styled.footer`
     a {
       margin-right: 0.2em;
       font-weight: 800;
-      &:nth-child(1) {
+      &:nth-of-type(1) {
         margin-left: 0.2em;
       }
     }
