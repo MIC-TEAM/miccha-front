4cd93b191783ed57dbf57d150e23f99d726c8c0b
devho813 <devho813@gmail.com>
2021-07-19 22:19:24 +0900

    add : 카테고리 페이지 예고 문구 추가

--- src/components/common/DefaultSlider/index.tsx
+++ src/components/common/DefaultSlider/index.tsx
@@ -1,5 +1,25 @@
+import styled from '@emotion/styled'
+
 const DefaultSlider = () => {
-  return <div>hi</div>
+  return (
+    <DefaultSliderContainer>
+      <h1>Comming Soon..</h1>
+    </DefaultSliderContainer>
+  )
 }
 
 export default DefaultSlider
+
+const DefaultSliderContainer = styled.div`
+  width: 100%;
+  height: 100%;
+
+  h1 {
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    font-size: 1.5vw;
+    font-weight: 700;
+  }
+`
